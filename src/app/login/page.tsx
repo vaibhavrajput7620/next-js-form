b05f990e6c2e390feae3bff895e319c8957aa360
vaibhavrajput7620 <156161834+vaibhavrajput7620@users.noreply.github.com>
2024-06-21 18:14:53 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * The function exports a React component that renders a Login component.
+ */
+import Login from "@/components/Login";
+
+
+const page = () => {
+  return <Login />
+};
+
+export default page;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * This function returns a page component that renders a Signup component.
+ */
+import Signup from "@/components/Signup";
+
+const page = () => {
+  return <Signup />
+};
+
+export default page;
